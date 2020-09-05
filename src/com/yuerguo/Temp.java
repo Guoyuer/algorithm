e5a943c26ac1b3a02313cdc2586f77261a879f04
Yuer Guo <2268169403@qq.com>
2020-09-05 18:41:38 +0800

    init

---
+++ src/com/yuerguo/Temp.java
@@ -0,0 +1,10 @@
+package com.yuerguo;
+
+public class Temp {
+    public static void main(String args[]) {
+        String s = "test.com";
+        char result = s.charAt(1);
+        System.out.println(result);
+    }
+
+}
